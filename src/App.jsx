d1490231370a334d76c9df35e9fdb99c6705cda5
Potchiiii <hassandaud470@gmail.com>
2025-02-20 22:41:07 +0800

    added about me section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Hero from './components/Hero'
-
+import AboutMe from './components/aboutMe/aboutMe';
+import './App.css';
 
 function App(){
   return(
     <div classname="App">
       <Hero />
+      <AboutMe />
     </div>
   )
 } 
